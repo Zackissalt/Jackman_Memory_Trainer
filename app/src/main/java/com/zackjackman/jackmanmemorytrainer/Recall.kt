cdaadc36fb171a0e2c74a36da6dfdfa628d67e79
Zackissalt <zackissalt@gmail.com>
2020-05-15 14:23:13 -0700

    added Recall activity

---
+++ app/src/main/java/com/zackjackman/jackmanmemorytrainer/Recall.kt
@@ -0,0 +1,12 @@
+package com.zackjackman.jackmanmemorytrainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recall : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recall)
+    }
+}
