037a9885fa3c8b2412c27f5350462802147b2431
Zackissalt <zackissalt@gmail.com>
2020-05-15 15:22:29 -0700

    added Layout for launch activity

--- app/src/main/java/com/zackjackman/jackmanmemorytrainer/MainActivity.kt
+++ app/src/main/java/com/zackjackman/jackmanmemorytrainer/MainActivity.kt
@@ -2,11 +2,19 @@ package com.zackjackman.jackmanmemorytrainer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val difficultys = resources.getStringArray(R.array.difficulty)
+        val menu = Spinner(findViewById(R.id.difficulty))
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, difficultys )
+        menu.adapter = adapter
+
     }
 }
