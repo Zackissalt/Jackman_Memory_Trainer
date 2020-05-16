60fc9fa084a1ec47d3e0487c80769a6852245fb4
Zackissalt <zackissalt@gmail.com>
2020-05-16 16:43:22 -0700

    build colorView Adapter to change circles colors

---
+++ app/src/main/java/com/zackjackman/jackmanmemorytrainer/ColorViewAdapter.kt
@@ -0,0 +1,35 @@
+package com.zackjackman.jackmanmemorytrainer
+
+import android.content.Context
+import android.graphics.PorterDuff
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.color_circle.view.*
+
+class ColorViewAdapter(val context: Context, val color: ArrayList<Int>) : BaseAdapter() {
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        val circle = color[p0]
+        val inflator = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflator.inflate(R.layout.color_circle, null)
+        view.circle.drawable.setColorFilter(circle, PorterDuff.Mode.SRC_IN)
+        return view
+    }
+
+    override fun getItem(p0: Int): Any {
+        return color[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getCount(): Int {
+        return color.size
+    }
+
+
+}
