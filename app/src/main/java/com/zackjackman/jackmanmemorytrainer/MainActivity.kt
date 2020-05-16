44cf449db8ab43daa7fc81e3f51618ef86612169
Zackissalt <zackissalt@gmail.com>
2020-05-16 15:25:53 -0700

    added and on click to move to the memorize activity

--- app/src/main/java/com/zackjackman/jackmanmemorytrainer/MainActivity.kt
+++ app/src/main/java/com/zackjackman/jackmanmemorytrainer/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.zackjackman.jackmanmemorytrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
         val menu :Spinner = findViewById(R.id.optionsMenu)
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, difficulties )
         menu.adapter = adapter
+
+        val start :Button = findViewById(R.id.startButton)
+        start.setOnClickListener {
+            val intent = Intent(this, Memorize::class.java)
+            intent.putExtra("difficulty", menu.selectedItemPosition )
+            this.startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/zackjackman/jackmanmemorytrainer/Memorize.kt
+++ app/src/main/java/com/zackjackman/jackmanmemorytrainer/Memorize.kt
@@ -14,6 +14,7 @@ class Memorize : AppCompatActivity() {
         setContentView(R.layout.activity_memorize)
 
 
+
     }
 
 }
