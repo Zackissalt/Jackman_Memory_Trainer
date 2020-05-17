a4e96dffcee942671c4b46f69379ad8dd7404a18
Zackissalt <zackissalt@gmail.com>
2020-05-17 14:18:43 -0700

    added evaluation to activity

--- app/src/main/java/com/zackjackman/jackmanmemorytrainer/Recall.kt
+++ app/src/main/java/com/zackjackman/jackmanmemorytrainer/Recall.kt
@@ -1,21 +1,31 @@
 package com.zackjackman.jackmanmemorytrainer
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_recall.*
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class Recall : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recall)
-
-        val colors : ArrayList<Int>  = (intent.extras.getBundle("bundle").getIntegerArrayList("colors"))
+        val bundle : Bundle = intent.extras.getBundle("bundle")
+        val colorNumbers : ArrayList<Int> = bundle.getIntegerArrayList("colorsNumbers")
+        val colors = ColorGenerator().getColorIdList(colorNumbers)
         val adapter = ColorSpinnerAdapter(this, colors )
         colorGrid.adapter = adapter
-
+        val evaluator = Evaluator(colorGrid, colorNumbers)
+        val commit : Button = findViewById(R.id.commit)
+        commit.setOnClickListener {
+            findViewById<TextView>(R.id.match).text = evaluator.getPercentRight().toString()  + "%"
+            commit.setText(R.string.again)
+        }
 
     }
 }
