042181aaa9908af549cad505d7025e8f01653856
Zackissalt <zackissalt@gmail.com>
2020-05-17 08:44:09 -0700

    fixed small errors

--- app/src/main/java/com/zackjackman/jackmanmemorytrainer/Memorize.kt
+++ app/src/main/java/com/zackjackman/jackmanmemorytrainer/Memorize.kt
@@ -1,12 +1,15 @@
 package com.zackjackman.jackmanmemorytrainer
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.GridView
+import android.os.CountDownTimer
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_memorize.*
-import kotlinx.android.synthetic.main.activity_recall.*
 
+
+@Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class Memorize : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,8 +18,23 @@ class Memorize : AppCompatActivity() {
 
         val difficulty  = (intent.extras.getInt("difficulty") + 1) * 3
         val colors = ColorGenerator(difficulty)
-        val adapter = ColorViewAdapter(this, colors.getColorIdList())
+        val colorsId= colors.getColorIdList()
+        val adapter = ColorViewAdapter(this, colorsId )
         colorGrid.adapter = adapter
+        val intent = Intent(this, Recall::class.java)
+        intent.putExtra("colors", colorsId  )
+
+        val countdown = object : CountDownTimer(30000,1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft  = (millisUntilFinished/1000).toString()
+                findViewById<TextView>(R.id.timer).text = timeLeft
+            }
+            override fun onFinish() {
+                this@Memorize.startActivity(intent)
+            }
+
+        }
+        countdown.start()
 
 
 
