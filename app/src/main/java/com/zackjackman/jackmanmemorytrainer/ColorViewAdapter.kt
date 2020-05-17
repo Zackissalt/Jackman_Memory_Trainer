fd6793a6dced2cfa5094b5a1db18e57ef3e34343
Zackissalt <zackissalt@gmail.com>
2020-05-17 07:28:47 -0700

    fixed issue with colors not coming out right

--- app/src/main/java/com/zackjackman/jackmanmemorytrainer/ColorViewAdapter.kt
+++ app/src/main/java/com/zackjackman/jackmanmemorytrainer/ColorViewAdapter.kt
@@ -1,12 +1,15 @@
 package com.zackjackman.jackmanmemorytrainer
 
 import android.content.Context
+import android.graphics.Color
 import android.graphics.PorterDuff
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import android.widget.BaseAdapter
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.toColor
 import kotlinx.android.synthetic.main.color_circle.view.*
 
 class ColorViewAdapter(val context: Context, val color: ArrayList<Int>) : BaseAdapter() {
@@ -15,7 +18,7 @@ class ColorViewAdapter(val context: Context, val color: ArrayList<Int>) : BaseAd
         val circle = color[p0]
         val inflator = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = inflator.inflate(R.layout.color_circle, null)
-        view.circle.drawable.setColorFilter(circle, PorterDuff.Mode.SRC_IN)
+        view.circle.setColorFilter(ContextCompat.getColor(context, circle ))
         return view
     }
 
