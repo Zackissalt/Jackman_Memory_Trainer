5b2d38606b74b475472d0b6d25cb87e89ac38e68
Zackissalt <zackissalt@gmail.com>
2020-05-17 11:05:25 -0700

    added logic for filling colorGrid in recall

--- app/src/main/java/com/zackjackman/jackmanmemorytrainer/Recall.kt
+++ app/src/main/java/com/zackjackman/jackmanmemorytrainer/Recall.kt
@@ -1,12 +1,21 @@
 package com.zackjackman.jackmanmemorytrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_recall.*
 
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class Recall : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recall)
+
+        val colors : ArrayList<Int>  = (intent.extras.getBundle("bundle").getIntegerArrayList("colors"))
+        val adapter = ColorSpinnerAdapter(this, colors )
+        colorGrid.adapter = adapter
+
+
     }
 }
